b7a1b748d01725f31dc260ed4394324f601d6eef
ael-kadh <ael-kadh@student.42.fr>
2016-08-04 10:34:32 +0100

    beginning

---
+++ Assets/Scripts/RotateCard.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateCard : MonoBehaviour {
+
+	private bool turn = false;
+
+	void OnMouseDown () {
+		print (transform.rotation.eulerAngles.y);
+		turn = true;
+	}
+
+	void Update() {
+		if (turn) {
+			if (transform.rotation.eulerAngles.y < 180)
+				transform.Rotate (0, 75 * Time.deltaTime, 0);
+			else
+				turn = false;
+		}
+	}
+}
